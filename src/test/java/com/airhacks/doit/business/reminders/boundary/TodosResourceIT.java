5c592d02a5d9901739e92ea551fe584eea7ac275
mehran_d <mehdow@gmail.com>
2016-06-12 12:05:31 +1000

    After adding Repository Library

--- src/test/java/com/airhacks/doit/business/reminders/boundary/TodosResourceIT.java
+++ src/test/java/com/airhacks/doit/business/reminders/boundary/TodosResourceIT.java
@@ -7,7 +7,12 @@ package com.airhacks.doit.business.reminders.boundary;
 
 import com.airhacks.rulz.jaxrsclient.JAXRSClientProvider;
 import static com.airhacks.rulz.jaxrsclient.JAXRSClientProvider.buildWithURI;
+import java.math.BigDecimal;
+import javax.json.Json;
+import javax.json.JsonArray;
 import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import static org.hamcrest.CoreMatchers.is;
@@ -23,7 +28,7 @@ import org.junit.Test;
 public class TodosResourceIT {
     
     @Rule
-    public JAXRSClientProvider prov = buildWithURI("http://localhost:8080/doit/api/todos");
+    public JAXRSClientProvider prov = buildWithURI("http://localhost:8080/doit/api/todos/");
     /*
     private Client client;
     private WebTarget tut;
@@ -31,16 +36,34 @@ public class TodosResourceIT {
     @Before
     public void initClient(){
         this.client = ClientBuilder.newClient();
-        this.tut = this.client.target("http://localhost:8080/doit/api/todos");
+        this.tut = this.client.target("http://localhost:11080/doit/api/todos");
     }
     //*/
     @Test
-    public void fetchToDos() {
+    public void crud() {
+        JsonObjectBuilder todoBuilder = Json.createObjectBuilder();
+        JsonObject todoToCreate = todoBuilder.
+                add("caption", "implement").
+                add("priority", 42).
+                add("description", "mehran added description").
+                build();
+        this.prov.target().request().post(Entity.json(todoToCreate));
+        //this.prov.target().request().post(Entity.json(todoToCreate));
+        Response pr = this.prov.target().request(MediaType.APPLICATION_JSON).get();
+        assertThat(pr.getStatus(), is(204));
+        
         Response res = this.prov.target().request(MediaType.APPLICATION_JSON).get();
         assertThat(res.getStatus(), is(200));
-        JsonObject payLoad = res.readEntity(JsonObject.class);
-        System.out.println(payLoad);
-        assertTrue(payLoad.getString("caption").startsWith("impl"));
+        //JsonObject td = res.readEntity(JsonObject.class);
+        JsonArray a = res.readEntity(JsonArray.class);
+        //System.out.println(payLoad);
+        JsonObject td = a.getJsonObject(0);
+        assertTrue(td.getString("caption").startsWith("impl"));
+        td = this.prov.target().path("42").request(MediaType.APPLICATION_JSON).get(JsonObject.class);
+        
+        assertTrue(td.getString("caption").contains("42"));
+        res = this.prov.target().path("42").request(MediaType.APPLICATION_JSON).delete();
+        assertThat(res.getStatus(), is(204));
     }
     
 }
